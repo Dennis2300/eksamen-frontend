da16f1520df38f7cb6506ac85c31f54857b3d922
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2024-06-20 18:13:55 +0200

    added admin page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Route, Routes } from "react-router-dom";
 //import Recipes from "./recipes/RecipeList";
 import Layout from "./Layout";
 import Home from "./Home";
-import Login from "./pages/Login";
+import Login from "./security/Login";
+import AdminPage from "./pages/AdminPage";
 import "./App.css";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Layout>
